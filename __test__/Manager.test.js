0813c0b05f6e6c21704738dae711b6de548bb161
Daniel Carazo <danielcarazo20@gmail.com>
2021-07-24 16:49:18 -0400

    create manager object/constructor

---
+++ __test__/Manager.test.js
@@ -0,0 +1,22 @@
+const Manager = require('../lib/Manager');
+
+test('create a new manager object', () => {
+    const manager = new Manager('Daniel');
+
+    expect(manager.name).toBe('Daniel');
+    // expect(manager.id).toEqual(expect.any(String));
+    expect(manager.email).toEqual(expect.any(String));
+})
+
+test('check office number', () => {
+    const manager = new Manager('Daniel');
+
+    expect(manager.getOfficeNumber()).toEqual(expect.any(String));
+})
+
+test('check manager role to be manager', () => {
+    const manager = new Manager('Daniel');
+
+    expect(manager.getRole()).toBe('Manager');
+})
+
