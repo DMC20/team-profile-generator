ec27a7514ed5bddb0629f13248a4ec2056e7791a
Daniel Carazo <danielcarazo20@gmail.com>
2021-07-29 09:42:04 -0400

    created generate html js file

---
+++ src/generateHTML.js
@@ -0,0 +1,17 @@
+// create function to export employee roles 
+
+const createMgr = function (manager) {
+    return ``
+}
+
+const createEng = function (engineer) {
+    return ``
+}
+
+const createInt = function (intern) {
+    return ``
+}
+
+generateHTML = (data) => {
+    
+}
